1cc1f13adec232c8efc08e8c68915776db670044
ivldheeraj <ivldheeraj@gmail.com>
2021-01-09 14:30:38 +0530

    TDD

---
+++ src/Lab2Arrays/SecondSmallest.java
@@ -0,0 +1,23 @@
+
+package com.cg.arrayslabwork;
+import java.util.Arrays;
+
+
+	public class SecondSmallest 
+	{
+			public int getSecondSmallest(int[] newNumbers) {
+		if(newNumbers.length==0) {
+			return 0;
+		}
+		else if(newNumbers.length==2) {
+			return Math.max(newNumbers[0], newNumbers[1]);
+		}
+		else if(newNumbers.length>2) {
+			Arrays.sort(newNumbers);
+			return newNumbers[1];
+		}
+		
+		return 1;
+	}
+
+	}
---
+++ src/Lab2Arrays/Sorted.java
@@ -0,0 +1,16 @@
+package com.cg.arrayslabwork;
+import java.util.Arrays;
+
+public class Sorted {
+
+	public int [] sortArray(int[] numbers) {
+		if(numbers.length==0) {
+			return numbers;
+		}
+		else if(numbers.length==1) {
+			return numbers;
+		}
+		return numbers;
+	}
+}
+
